139d2c05f9ac808a5633dc68d8b145d43bd5209b
Bugra <seyitbugraerden@gmail.com>
2024-01-28 11:09:05 +0300

    More Clean Code

--- src/App.jsx
+++ src/App.jsx
@@ -8,167 +8,140 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [selectedData, setSelectedData] = useState([]);
 
-  const onbirea = [
-    "OZAN ATA DURAK",
-    "HİRA BOYRAZ",
-    "İLAYDA SÜMEYRA ÖNDER",
-    "EGE YELTEKİN",
-    "SELVİNUR ÖZCAN",
-    "DEFNE TANRIVERDİ",
-  ];
-  const onbirsaya = [
-    "AYŞENUR ÜLGER",
-    "NESLİHAN ŞAHİN",
-    "KUZEY TANRIVERDİ",
-    "CAHİT EFE ŞSHİN",
-    "ŞEVVAL ECEM ÖZDEMİR",
-    "YİĞİT EFE SEYLAN",
-    "AHMET YAVUZHAN FINDIK",
-    "MEHLİKA REYYAN ÖZDEMİR",
-    "MUHAMMED YİĞİT AĞIR",
-    "MUHAMMMED EREN BABAYİĞİT",
-    "ECENAZ TAYFUN",
-    "IRMAK AKSOY",
-    "FİRDEVS NUR COŞAN",
-  ];
-  const onbirsayb = [
-    "ELİF ŞİMŞEK",
-    "TUĞBA DEFNE ÖZDEMİR",
-    "MEHMET ASLAN",
-    "MELİH KEREM ÜLGER",
-    "CEYDA DEMİRÖS",
-    "ELİF SELEN ESER",
-    "SEFA ÇAĞRI ALACAHAN",
-    "YAĞMUR AZRA ÇELEBİ",
-    "HİLAL ÜLKER",
-    "ENES YEŞİLDAL",
-    "ERBERK HİKMET",
-    "MUSTAFA SALİHCAN DALKIRAN",
-  ];
-  const onikiea = [
-    "MUSTAFA SELİM KOÇ",
-    "BEYZA NUROĞLU",
-    "İBRAHİM ALP TAŞKIRAN",
-    "MEHLİKA KORKUTATA",
-    "SİNAN GEZER",
-    "ELANUR ALTUNTAŞ",
-    "SEDA AKYÜREK",
-    "MUHAMMED GÜNEY",
-    "NAZ YILMAZ",
-    "EBRAR TUKAT",
-    "HATİCE KÜBRA DEMİRAL",
-  ];
-  const onikisaya = [
-    "ASYA SANEM SAYLAN",
-    "SUDENAZ KILIÇ",
-    "CEMRE NAZ ÖZKAN",
-    "ESMA NUR MERCAN",
-    "HALİS SERKAN ÇAKMAKCI",
-    "RANA ATALAY",
-    "FATMANUR ŞİRİN",
-  ];
-  const onikisayb = [
-    "ADNAN EMRE TAŞKIRAN",
-    "TUANA ÖZDEMİR",
-    "ÖMER MERT AYAZ",
-    "TAHSİN KOCAOĞLU",
-    "ECRİN YAĞMUR",
-    "ZEYNEP SÜMEYYE AKGÜN",
-    "ELİF EBRAR AKSOY",
-  ];
-  const msaya = [
-    "MUHAMMED SALİH DURGUT",
-    "EFE SARI",
-    "SALİH EMİN ÜNAL",
-    "ÖMER FARUK AVAL",
-    "ZEHRA MEKİK",
-    "SUDENUR KAMACI",
-    "İREM YAĞMUR ACAR",
-    "SEDEN DOĞAN",
-    "MELİH ARIDURU",
-    "RAVZA NUR ALPAGUT",
-    "BEKİR KERİM EKTİ",
-    "BAYRAM BERAT AKAR",
-    "METİN KILIÇ",
-    "ŞÜHEDA ILICA",
-    "BETÜL İLKİM KARTAL",
-  ];
-  const msayb = [
-    "NİDANUR AKÇAN",
-    "ŞİAR ABDURRAHMAN ÇELİK",
-    "İREM BOZKURT",
-    "MUHAMMED YAHYA KUTOĞLU",
-    "EREN GÖDEN",
-    "NAZLI SENA ÇELİKER",
-    "ELİF EYLÜL DURMAZ",
-    "EMİRHAN KARAMAN",
-    "SİDELYA ŞİMAN",
-    "SUDE GÜL",
-    "AHMET KARADÖL",
-  ];
-  const meaa = [
-    "CEYLİN KAPLAN",
-    "AZRA KARABAYIR",
-    "ARZUM NUR ARSLAN",
-    "ZEREN NURUAN",
-    "GÖZDE KILIÇER",
-    "AHMET KASIM",
-    "MERVE AYDIN",
-    "RÜVEYDA İPEK TOPAÇ",
-    "ELİF BEYZA GÖKÇE",
-    "DAMLA SÜRÜCÜOĞLU",
-    "GÜL AYANOĞLU",
-  ];
-  const meab = [
-    "KAAN ARSLAN UZUNOĞLU",
-    "BELİNAY DEMİROĞLU",
-    "İSMAİL MEHMET MERT CİVAN",
-    "EFE HALİT DEMİRCİ",
-    "AHMET CAN MERCAN",
-    "MUHAMMET SALİH AYDIN",
-    "RANA BERİN IRMAK",
-    "FEYZA NUR DEMİRTAŞ",
-    "HAMDİ ARDA AKIN",
-    "EFİL TUANA ŞAHİNDOKUYUCU",
-    "MELİSA KAPLAN",
-    "KAĞAN AKAR",
-  ];
+  const ClassData = {
+    onbirea: [
+      "OZAN ATA DURAK",
+      "HİRA BOYRAZ",
+      "İLAYDA SÜMEYRA ÖNDER",
+      "EGE YELTEKİN",
+      "SELVİNUR ÖZCAN",
+      "DEFNE TANRIVERDİ",
+    ],
+    onbirsaya: [
+      "AYŞENUR ÜLGER",
+      "NESLİHAN ŞAHİN",
+      "KUZEY TANRIVERDİ",
+      "CAHİT EFE ŞSHİN",
+      "ŞEVVAL ECEM ÖZDEMİR",
+      "YİĞİT EFE SEYLAN",
+      "AHMET YAVUZHAN FINDIK",
+      "MEHLİKA REYYAN ÖZDEMİR",
+      "MUHAMMED YİĞİT AĞIR",
+      "MUHAMMMED EREN BABAYİĞİT",
+      "ECENAZ TAYFUN",
+      "IRMAK AKSOY",
+      "FİRDEVS NUR COŞAN",
+    ],
+    onbirsayb: [
+      "ELİF ŞİMŞEK",
+      "TUĞBA DEFNE ÖZDEMİR",
+      "MEHMET ASLAN",
+      "MELİH KEREM ÜLGER",
+      "CEYDA DEMİRÖS",
+      "ELİF SELEN ESER",
+      "SEFA ÇAĞRI ALACAHAN",
+      "YAĞMUR AZRA ÇELEBİ",
+      "HİLAL ÜLKER",
+      "ENES YEŞİLDAL",
+      "ERBERK HİKMET",
+      "MUSTAFA SALİHCAN DALKIRAN",
+    ],
+    onikiea: [
+      "MUSTAFA SELİM KOÇ",
+      "BEYZA NUROĞLU",
+      "İBRAHİM ALP TAŞKIRAN",
+      "MEHLİKA KORKUTATA",
+      "SİNAN GEZER",
+      "ELANUR ALTUNTAŞ",
+      "SEDA AKYÜREK",
+      "MUHAMMED GÜNEY",
+      "NAZ YILMAZ",
+      "EBRAR TUKAT",
+      "HATİCE KÜBRA DEMİRAL",
+    ],
+    onikisaya: [
+      "ASYA SANEM SAYLAN",
+      "SUDENAZ KILIÇ",
+      "CEMRE NAZ ÖZKAN",
+      "ESMA NUR MERCAN",
+      "HALİS SERKAN ÇAKMAKCI",
+      "RANA ATALAY",
+      "FATMANUR ŞİRİN",
+    ],
+    onikisayb: [
+      "ADNAN EMRE TAŞKIRAN",
+      "TUANA ÖZDEMİR",
+      "ÖMER MERT AYAZ",
+      "TAHSİN KOCAOĞLU",
+      "ECRİN YAĞMUR",
+      "ZEYNEP SÜMEYYE AKGÜN",
+      "ELİF EBRAR AKSOY",
+    ],
+    msaya: [
+      "MUHAMMED SALİH DURGUT",
+      "EFE SARI",
+      "SALİH EMİN ÜNAL",
+      "ÖMER FARUK AVAL",
+      "ZEHRA MEKİK",
+      "SUDENUR KAMACI",
+      "İREM YAĞMUR ACAR",
+      "SEDEN DOĞAN",
+      "MELİH ARIDURU",
+      "RAVZA NUR ALPAGUT",
+      "BEKİR KERİM EKTİ",
+      "BAYRAM BERAT AKAR",
+      "METİN KILIÇ",
+      "ŞÜHEDA ILICA",
+      "BETÜL İLKİM KARTAL",
+    ],
+    msayb: [
+      "NİDANUR AKÇAN",
+      "ŞİAR ABDURRAHMAN ÇELİK",
+      "İREM BOZKURT",
+      "MUHAMMED YAHYA KUTOĞLU",
+      "EREN GÖDEN",
+      "NAZLI SENA ÇELİKER",
+      "ELİF EYLÜL DURMAZ",
+      "EMİRHAN KARAMAN",
+      "SİDELYA ŞİMAN",
+      "SUDE GÜL",
+      "AHMET KARADÖL",
+    ],
+    meaa: [
+      "CEYLİN KAPLAN",
+      "AZRA KARABAYIR",
+      "ARZUM NUR ARSLAN",
+      "ZEREN NURUAN",
+      "GÖZDE KILIÇER",
+      "AHMET KASIM",
+      "MERVE AYDIN",
+      "RÜVEYDA İPEK TOPAÇ",
+      "ELİF BEYZA GÖKÇE",
+      "DAMLA SÜRÜCÜOĞLU",
+      "GÜL AYANOĞLU",
+    ],
+    meab: [
+      "KAAN ARSLAN UZUNOĞLU",
+      "BELİNAY DEMİROĞLU",
+      "İSMAİL MEHMET MERT CİVAN",
+      "EFE HALİT DEMİRCİ",
+      "AHMET CAN MERCAN",
+      "MUHAMMET SALİH AYDIN",
+      "RANA BERİN IRMAK",
+      "FEYZA NUR DEMİRTAŞ",
+      "HAMDİ ARDA AKIN",
+      "EFİL TUANA ŞAHİNDOKUYUCU",
+      "MELİSA KAPLAN",
+      "KAĞAN AKAR",
+    ],
+  };
   const handleDataSelected = (data) => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
       setIsTurned(false);
     }, 500);
-    if (data === "meab") {
-      setSelectedData(meab);
-    }
-    if (data === "msayb") {
-      setSelectedData(msayb);
-    }
-    if (data === "meaa") {
-      setSelectedData(meaa);
-    }
-    if (data === "msaya") {
-      setSelectedData(msaya);
-    }
-    if (data === "onikisayb") {
-      setSelectedData(onikisayb);
-    }
-    if (data === "onikisaya") {
-      setSelectedData(onikisaya);
-    }
-    if (data === "onikiea") {
-      setSelectedData(onikiea);
-    }
-    if (data === "onbirsayb") {
-      setSelectedData(onbirsayb);
-    }
-    if (data === "onbirsaya") {
-      setSelectedData(onbirsaya);
-    }
-    if (data === "onbirea") {
-      setSelectedData(onbirea);
-    }
+    setSelectedData(ClassData[data]);
   };
 
   return (
